f00b1c7ea3368e62e5eb7a5aa68f35fb20242359
beka <beka-nishanbek@mail.ru>
2026-02-07 18:34:56 +0600

    fix

--- server.js
+++ server.js
@@ -30,6 +30,10 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 3000;
 
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("Listening on", PORT);
+});
+
 connectDB(process.env.MONGODB_URI)
   .then(() => {
     app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
